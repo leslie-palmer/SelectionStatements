6a3a15a15bc039124a7f7ed90364dfa7e339dd81
leslie-palmer <scoopicus@gmail.com>
2022-06-03 01:40:11 -0700

    added comments

--- SelectionStatements/Program.cs
+++ SelectionStatements/Program.cs
@@ -6,12 +6,13 @@ namespace SelectionStatements
     {
         static void Main(string[] args)
         {
+            //Creates Random r, assigns a random number to favNumber
             var r = new Random();
             var favNumber = r.Next(1, 1000);
-
+            //Assigns an int from user to userInput
             Console.WriteLine("Try to guess my favorite number");
             var userInput = int.Parse(Console.ReadLine());
-
+            //Compares userInput to favNumber and gives output based on result
             if (userInput < favNumber)
             {
                 Console.WriteLine("Too low");
